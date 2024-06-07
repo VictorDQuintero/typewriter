30df1548ef7bb5da67864a242b99b579009c3004
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-06 19:24:28 -0600

    Add comments to code

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,12 @@
 const sentence = "hello there from lighthouse labs";
 
 for (let i = 0; i < sentence.length; i++) {
-  let delayTime = i * 50;
+  // loops through string
+  let delayTime = i * 50; // calculates a delay time for each element of string
   setTimeout(() => {
-    process.stdout.write(sentence[i]);
+    process.stdout.write(sentence[i]); // prints each element of string
     if (i === sentence.length - 1) {
+      // makes new line in terminal
       console.log("");
     }
   }, delayTime);
