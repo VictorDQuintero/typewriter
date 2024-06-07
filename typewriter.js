261864e0b2c52bb784629afea4b63410ea01ed21
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-06 19:21:55 -0600

    Add code to typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+  let delayTime = i * 50;
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+    if (i === sentence.length - 1) {
+      console.log("");
+    }
+  }, delayTime);
+}
